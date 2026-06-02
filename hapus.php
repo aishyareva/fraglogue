71ee919557ac88c7d1fc36dd8dafc48507d427d8
aishyareva <aishyareva@gmail.com>
2026-06-02 18:48:38 +0700

    Update hapus.php

--- hapus.php
+++ hapus.php
@@ -3,9 +3,19 @@ session_start();
 if (!isset($_SESSION['login']) || $_SESSION['role'] !== 'admin') { header("Location: index.php"); exit; }
 include 'koneksi.php'; 
 
-$id = mysqli_real_escape_string($koneksi, $_GET['id']); // [cite: 168]
+$id = mysqli_real_escape_string($koneksi, $_GET['id']);
 
-mysqli_query($koneksi, "DELETE FROM perfumes WHERE id_perfume = '$id'"); // [cite: 169]
+$query = mysqli_query($koneksi, "SELECT image_url FROM perfumes WHERE id_perfume = '$id'");
+$data = mysqli_fetch_array($query);
+
+if ($data) {
+    $image_path = $data['image_url'];
+    if (file_exists($image_path) && strpos($image_path, 'http') === false) {
+        unlink($image_path);
+    }
+}
+
+mysqli_query($koneksi, "DELETE FROM perfumes WHERE id_perfume = '$id'");
 
 header("Location: index.php"); 
 exit;
